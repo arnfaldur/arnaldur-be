306b0ed4100dd49272fe989ceae785a379b33101
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2023-06-03 16:18:25 +0200

    replaced index.mdx with index.tsx

---
+++ src/routes/index.tsx
@@ -0,0 +1,20 @@
+import { Title } from "solid-start";
+import Counter from "~/components/Counter";
+
+export default function Home() {
+  return (
+    <>
+      <Title>Hello World</Title>
+      <h1>Hello world!</h1>
+      <Counter />
+      <p>
+        Visit{" "}
+        <a href="https://start.solidjs.com" target="_blank">
+          start.solidjs.com
+        </a>{" "}
+        to learn how to build SolidStart apps.
+        lasdkjflsakdafj
+      </p>
+    </>
+  );
+}
