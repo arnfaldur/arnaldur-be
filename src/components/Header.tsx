e1ef1b8c49a7a9ea50238b397c4fd55d7aa8c58d
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2023-07-04 23:26:31 +0200

    work around a probable solid-js/start bug

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,7 @@ export default function Header() {
   const location = useLocation();
   const url = createMemo(() => "Arnaldur.be" + location.pathname);
 
+  // construct each url of the breadcrumb and it's least significant slug
   const partUrls = createMemo(() =>
     location.pathname.split("/").slice(1).reduce((prev: Array<{ part: string, url: string }>, cur, i) =>
       prev.concat({ part: cur, url: (i > 0 ? prev[i - 1].url : "") + "/" + cur }),
@@ -20,7 +21,7 @@ export default function Header() {
           <A href="/explaining-the-url">be</A>
           <For each={partUrls()}>
             {(part) => (<>
-              /<A href={part.url}>{part.part}</A>
+              <span>/</span><A href={part.url}>{part.part}</A>
             </>)}
           </For>
         </h1>
