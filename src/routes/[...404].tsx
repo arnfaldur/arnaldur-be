1af34fc2de8e6d5262aab8bb571e646a8ad6b7bb
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2024-05-12 01:07:51 +0200

    404 page is more visually consistent

---
+++ src/routes/[...404].tsx
@@ -0,0 +1,12 @@
+import Header from "~/components/Header";
+
+export default function PageNotFoundRoute() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1> 404: Page Not Found </h1>
+      </main>
+    </>
+  );
+}
