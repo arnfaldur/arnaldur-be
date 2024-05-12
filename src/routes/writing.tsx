bfc7416ccde9018a67186f1126c85fef7571e595
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2024-05-12 02:06:26 +0200

    removed dummy file, improved post hiding

--- src/routes/writing.tsx
+++ src/routes/writing.tsx
@@ -1,30 +1,39 @@
-
 import { createContext } from "solid-js";
 import { Outlet } from "solid-start";
 
 import Header from "~/components/Header";
 
-const posts = import.meta.glob<{
-  component: any, frontmatter: { title: string, date: string, hidden?: boolean }
+const unfilteredPosts = import.meta.glob<{
+  component: any,
+  frontmatter: { title: string, date: string, hidden?: boolean },
 }>("./writing/about/**/*.{md,mdx}", { eager: true });
 
-console.log('posts',posts);
-const entries: { [t: string]: { title: string, date: Date, hidden?: boolean } } = Object
-  .fromEntries(Object.entries(posts)
-    .filter(([post, { frontmatter }]) => !frontmatter?.hidden)
-    .map(([post, { frontmatter }]): [string, { title: string; date: Date; }] => (
-      [post.slice(1).replace(/\/\(.*\)\.mdx/, ""), {
+const posts = Object.fromEntries(
+  Object.entries(unfilteredPosts).filter(
+    ([post, { frontmatter }]) => !frontmatter?.hidden,
+  ),
+);
+
+const entries: {
+  [t: string]: { title: string, date: Date, hidden?: boolean },
+} = Object.fromEntries(
+  Object.entries(posts).map(
+    ([post, { frontmatter }]): [string, { title: string, date: Date }] => [
+      post.slice(1).replace(/\/\(.*\)\.mdx/, ""),
+      {
         title: frontmatter.title,
         date: new Date(frontmatter.date),
-      }]))
-  );
+      },
+    ],
+  ),
+);
 export const PostsContext = createContext(entries);
 
 export default function WritingLayout() {
   return (
     <PostsContext.Provider value={entries}>
       <Header />
-      <main >
+      <main>
         <Outlet />
       </main>
     </PostsContext.Provider>
