3e40a391a6f40d3d14531622478ebe4dcde8a6a6
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2024-05-19 14:57:23 +0200

    added remark embedder and smartypants

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "@solidjs/start/config";
 
-import pkg from "@vinxi/plugin-mdx";
-const { default: mdx } = pkg;
+import mdx from "@vinxi/plugin-mdx";
 
 import remarkGfm from "remark-gfm";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import remarkMath from "remark-math";
+import remarkEmbedder from "@remark-embedder/core";
+import remarkSmartypants from "remark-smartypants";
+import oEmbedTransformer from "@remark-embedder/transformer-oembed";
+import embedderCache from "@remark-embedder/cache";
 import rehypeMdxImportMedia from "rehype-mdx-import-media";
 import rehypeKatex from "rehype-katex";
 import rehypeSlug from "rehype-slug";
@@ -21,11 +24,22 @@ export default defineConfig({
   },
   vite: {
     plugins: [
-      mdx.withImports({})({
+      mdx.default.withImports({})({
         jsx: true,
         jsxImportSource: "solid-js",
         providerImportSource: "solid-mdx",
         remarkPlugins: [
+          [remarkSmartypants, { dashes: "oldschool" }],
+          [
+            remarkEmbedder.default,
+            {
+              embedderCache,
+              transformers: [
+                oEmbedTransformer.default,
+                // { params: { theme: "dark", dnt: true } },
+              ],
+            },
+          ],
           remarkGfm,
           remarkFrontmatter,
           remarkMdxFrontmatter,
