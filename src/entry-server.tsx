6a0e9d7304ad5320cdd62b073e34357181593a39
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2024-10-05 18:37:19 +0200

    added darkreader signal

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -11,6 +11,7 @@ export default createHandler(
                             name="viewport"
                             content="width=device-width, initial-scale=1"
                         />
+                        <meta name="darkreader-lock" />
                         <link
                             rel="icon"
                             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22><text y=%32px%22 font-size=%2232%22>🆎</text></svg>"
