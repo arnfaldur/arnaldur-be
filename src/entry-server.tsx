5257340e4d511d7bd575aa40bb000c34187ecec7
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2025-01-22 17:07:32 +0100

    directly embedded tweets, fixed katex.css link

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -18,7 +18,8 @@ export default createHandler(
                         />
                         <link
                             rel="stylesheet"
-                            href="https:cdn.jsdelivr.net/npm/katex@0.16.21/dist/katex.min.css"
+                            href="https://cdn.jsdelivr.net/npm/katex@0.16.21/dist/katex.min.css"
+                            type="text/css"
                             integrity="sha384-zh0CIslj+VczCZtlzBcjt5ppRcsAmDnRem7ESsYwWwg3m/OaJ2l4x7YBZl9Kxxib"
                             crossorigin="anonymous"
                         />
