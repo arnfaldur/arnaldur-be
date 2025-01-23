11b678c1ae7c1bb1901279413c83379f88b5f62a
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2025-01-23 19:32:30 +0100

    postList to front page & added favicon

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,28 +1,34 @@
 import { StartServer, createHandler } from "@solidjs/start/server";
 
+const favicon =
+    "data:image/svg+xml," +
+    "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'>" +
+    "<text y='77.5px' font-size='90'>🆎</text>" +
+    "</svg>";
+
+const Head = () => (
+    <>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="darkreader-lock" />
+        <link rel="icon" href={favicon} />
+        <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/katex@0.16.21/dist/katex.min.css"
+            type="text/css"
+            integrity="sha384-zh0CIslj+VczCZtlzBcjt5ppRcsAmDnRem7ESsYwWwg3m/OaJ2l4x7YBZl9Kxxib"
+            crossorigin="anonymous"
+        />
+    </>
+);
+
 export default createHandler(
     () => (
         <StartServer
             document={({ assets, children, scripts }) => (
                 <html lang="en">
                     <head>
-                        <meta charset="utf-8" />
-                        <meta
-                            name="viewport"
-                            content="width=device-width, initial-scale=1"
-                        />
-                        <meta name="darkreader-lock" />
-                        <link
-                            rel="icon"
-                            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22><text y=%32px%22 font-size=%2232%22>🆎</text></svg>"
-                        />
-                        <link
-                            rel="stylesheet"
-                            href="https://cdn.jsdelivr.net/npm/katex@0.16.21/dist/katex.min.css"
-                            type="text/css"
-                            integrity="sha384-zh0CIslj+VczCZtlzBcjt5ppRcsAmDnRem7ESsYwWwg3m/OaJ2l4x7YBZl9Kxxib"
-                            crossorigin="anonymous"
-                        />
+                        <Head />
                         {assets}
                     </head>
                     <body>
