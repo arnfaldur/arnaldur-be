322d94d358fd307708fbb4d0ae58445982abcc6f
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2025-03-25 13:39:40 +0100

    FFT: moved index file to tsx

---
+++ src/routes/playing/with/fourier-transforms/(fft).tsx
@@ -0,0 +1,14 @@
+import { DrawingCanvas } from "./canvas";
+
+export const title = "Inverse Fourier Transform";
+export const date = new Date("2025-03-22T12:42:22.878Z");
+export const topic = ["programming", "mathematics"];
+
+export default function Component() {
+	return (
+		<>
+			<h1>Inverse Fourier Transform</h1>
+			<DrawingCanvas />
+		</>
+	);
+}
