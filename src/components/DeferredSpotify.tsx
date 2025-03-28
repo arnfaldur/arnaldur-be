ce457ee5401e8b3d849f5a57511ea6aafc13d6f4
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2025-03-28 17:23:45 +0100

    enjoying: nightmares on wax les nuits

---
+++ src/components/DeferredSpotify.tsx
@@ -0,0 +1,40 @@
+import { createSignal, Show } from "solid-js";
+
+export default function DeferredSpotify(props: any) {
+    const [isOpen, setIsOpen] = createSignal(false);
+
+    const Placeholder = (
+        <div
+            onClick={() => setIsOpen(true)}
+            style={{
+                cursor: "pointer",
+                display: "flex",
+                "max-width": "100%",
+                "padding-bottom": "6.5px",
+                "align-items": "center",
+                "justify-content": "center",
+                "background-color": "black",
+                color: "white",
+                "border-radius": "8px",
+            }}
+        >
+            <span style={{ "font-size": "1.5em" }}>
+              {props.placeholder ?? "Click here to load youtube video"}
+            </span>
+        </div>
+    );
+
+    return (
+        <>
+            <Show when={isOpen()} fallback={Placeholder}>
+                <iframe
+                    src={props.url}
+                    title="Spotify player"
+                    allow="clipboard-write; encrypted-media; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin"
+                    loading="lazy"
+                />
+            </Show>
+        </>
+    );
+}
