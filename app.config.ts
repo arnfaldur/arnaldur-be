1fd5c47004e22f24b4f5832a50450cc5a5d47108
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2025-10-13 19:16:22 +0200

    fix: added root to prerenderd routes

--- app.config.ts
+++ app.config.ts
@@ -55,7 +55,7 @@ const server = {
         autoSubfolderIndex: false,
         concurrency: 4,
         failOnError: true,
-        routes: ["/writing", "/laughing", "/experimenting"],
+        routes: ["/", "/writing", "/laughing", "/experimenting"],
         crawlLinks: true,
     },
 };
